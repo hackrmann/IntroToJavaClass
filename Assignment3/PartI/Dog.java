d451e5aa745dfba461e132d50e45d27767e7f7a6
Thejus Rao <thejus44@gmail.com>
2023-02-10 21:29:54 -0500

    Added Assignment3

---
+++ Assignment3/PartI/Dog.java
@@ -0,0 +1,13 @@
+
+public class Dog {
+        private int age;
+        private String owner;
+        private String breed;
+        
+        public static void main(String[] args) {
+                
+                Dog[] dogs = new Dog[5];
+                dogs[0] = new Dog(4, "Stephen Colbert", "Boxer");
+                
+        }
+}
---
+++ Assignment3/PartI/SquarePyramid.java
@@ -0,0 +1,5 @@
+
+public class SquarePyramid {
+	double a;
+    double H;
+}
