15ac92dbf7a2a2c2a99d1e2980298a81e234df1a
Thejus Rao <thejus44@gmail.com>
2023-02-28 02:32:35 -0500

    Maybe finished assignment?

--- Assignment5/PartI/ListOfNumbers.java
+++ Assignment5/PartI/ListOfNumbers.java
@@ -2,13 +2,16 @@ import java.io.*;
 import java.util.List;
 import java.util.ArrayList;
 
+import vehicles.*;
+
 public class ListOfNumbers {
 
-    private ArrayList rdfTripleList;
+    private ArrayList<RDFTriple<Integer, Integer, Integer>> rdfTripleList;
     private String fileName;
 
-    public ListOfNumbers () {
+    public ListOfNumbers() {
         // create an ArrayList of Pairs of Integers
+        rdfTripleList = new ArrayList<RDFTriple<Integer, Integer, Integer>>();
     }
 
     public ArrayList getRdfTripleList() {
@@ -16,23 +19,52 @@ public class ListOfNumbers {
     }
 
     public void createList() {
-        for (int i = 0 ; i< 100 ; i++) {
-            Integer number1 = (int) (Math.random()*10000);
-            Integer number2 = (int) (Math.random()*10000);
-            Integer number3 = (int) (Math.random()*10000);
+        for (int i = 0; i < 100; i++) {
+            Integer number1 = (int) (Math.random() * 10000);
+            Integer number2 = (int) (Math.random() * 10000);
+            Integer number3 = (int) (Math.random() * 10000);
             // fill the existing list with RDFTriple objects
             // of three numbers.
+            RDFTriple<Integer, Integer, Integer> rdfTriple = new RDFTriple<>(number1, number2, number3);
+            rdfTripleList.add(rdfTriple);
         }
     }
 
 
-    public ListOfNumbers (String fileName) {
+    public ListOfNumbers(String fileName) {
         this();
         this.fileName = fileName;
     }
 
     public void readList() {
+        rdfTripleList = new ArrayList<>();
+        RDFTriple<Integer, Integer, Integer> rdfTriple = new RDFTriple<>(0, 0, 0);
+        String line = null;
+        String[] numbers = new String[3];
+        boolean continueReading = true;
+        File f = new File(this.fileName);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(f));
+            while (continueReading) {
+                try {
+                    line = bufferedReader.readLine();
+                    continueReading = (line != null);
+                    if (continueReading) {
+                        numbers = line.split(" ");
+                        rdfTriple.setSubj(Integer.parseInt(numbers[0]));
+                        rdfTriple.setPred(Integer.parseInt(numbers[1]));
+                        rdfTriple.setObj(Integer.parseInt(numbers[2]));
+                        rdfTripleList.add(rdfTriple);
+                    }
 
+                } catch (IOException ioe) {
+                    System.err.println("There was an I/O error while reading the line! Exiting");
+                }
+            }
+        } catch (FileNotFoundException fne) {
+            System.out.println("File " + fileName + " not found! Exiting");
+            System.exit(1);
+        }
     }
 
     public void writeList() {
@@ -41,7 +73,8 @@ public class ListOfNumbers {
             System.out.println("Entering try statement");
             out = new PrintWriter(new FileWriter(this.fileName));
             for (int i = 0; i < rdfTripleList.size(); i++)
-                out.println(rdfTripleList.get(i).getKey() + " " + rdfTripleList.get(i).getValue());
+                out.println(rdfTripleList.get(i).getSubj() + " "
+                        + rdfTripleList.get(i).getPred() + " " + rdfTripleList.get(i).getObj());
         } catch (IndexOutOfBoundsException e) {
             System.err.println("Caught IndexOutOfBoundsException: " +
                     e.getMessage());
@@ -57,7 +90,7 @@ public class ListOfNumbers {
         }
     }
 
-    public static void cat(String fileName) {
+    public static void cat(String fileName) throws IOException {
         RandomAccessFile input = null;
         String line = null;
         File file = new File(fileName);
@@ -67,6 +100,8 @@ public class ListOfNumbers {
                 System.out.println(line);
             }
             return;
+        } catch (IOException ioException) {
+
         } finally {
             if (input != null) {
                 input.close();
@@ -76,7 +111,11 @@ public class ListOfNumbers {
 
     public static void main(String[] args) {
         ListOfNumbers listOfNumbers = new ListOfNumbers("numberfile.txt");
-        ListOfNumbers.cat("numberfile.txt");
+        try {
+            ListOfNumbers.cat("numberfile.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         listOfNumbers.readList();
     }
 
