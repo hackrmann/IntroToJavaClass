865476fa8d69dc7470c912ab362ce856cadf00ab
Thejus Rao <thejus44@gmail.com>
2023-04-15 22:14:14 -0400

    Changed stuff

--- Assignment8/src/chat/ChatClient.java
+++ Assignment8/src/chat/ChatClient.java
@@ -1,26 +1,112 @@
 package chat;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 
 public class ChatClient extends JFrame implements Runnable {
 
-	private static int WIDTH = 400;
-	private static int HEIGHT = 300;
-	
-	public ChatClient() {
-		super("Chat Client");
-		this.setSize(ChatClient.WIDTH, ChatClient.HEIGHT);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
-		
-	}
-	
-	public void run() {
+    private static int WIDTH = 400;
+    private static int HEIGHT = 300;
 
-	}
-	
-	public static void main(String[] args) {
-		ChatClient chatClient = new ChatClient();
-	}
+    DataOutputStream toServer = null;
+    DataInputStream fromServer = null;
+    JTextField textField;
+    JTextArea textArea = null;
+    JScrollPane scroll;
+
+    Socket socket;
+
+    public ChatClient() {
+        super("Chat Client");
+
+        textField = new JTextField(5);
+        textArea = new JTextArea(30, 30);
+        textArea.setEditable(false);
+        this.setLayout(new BorderLayout());
+        textField.addActionListener(new TextFieldListener());
+        JPanel topPanel = new JPanel(new GridLayout(2, 1));
+        JPanel controlPanel = new JPanel();
+        scroll = new JScrollPane(textArea);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        //Add Textarea in to middle panel
+        controlPanel.add(scroll);
+        topPanel.add(controlPanel);
+        topPanel.add(textField);
+        this.add(textArea, BorderLayout.CENTER);
+        this.add(topPanel, BorderLayout.SOUTH);
+        createMenu();
+        this.setSize(ChatClient.WIDTH, ChatClient.HEIGHT);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+    }
+
+    private void createMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        JMenuItem connectItem = new JMenuItem("Connect");
+        JMenuItem exitItem = new JMenuItem("Exit");
+        connectItem.addActionListener((e) -> {
+            try {
+                socket = new Socket("localhost", 8000);
+                textArea.append("----------\nConnected!\n----------\n");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        exitItem.addActionListener((e) -> System.exit(1));
+        menu.add(connectItem);
+        menu.add(exitItem);
+        menuBar.add(menu);
+        this.setJMenuBar(menuBar);
+    }
+
+    public void run() {
+
+    }
+
+    class TextFieldListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(socket == null) {
+                textArea.append("Chatroom not connected to Server. Please connect to server and try again\n");
+            }
+            try {
+                // Create a socket to connect to the server
+                // Create an input stream to receive data from the server
+                fromServer = new DataInputStream(socket.getInputStream());
+                // Create an output stream to send data to the server
+                toServer = new DataOutputStream(socket.getOutputStream());
+            } catch (IOException ex) {
+                textArea.append("ERROR: " + ex.toString() + '\n');
+            }
+
+            try {
+                String message = textField.getText().trim();
+                toServer.writeUTF(message);
+                toServer.flush();
+                String getmessage = fromServer.readUTF();
+
+                textArea.append(getmessage + "\n");
+                textField.setText("");
+                //socket.close();
+            } catch (IOException ex) {
+                System.err.println(ex);
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ChatClient chatClient = new ChatClient();
+    }
 }
--- Assignment8/src/chat/ChatServer.java
+++ Assignment8/src/chat/ChatServer.java
@@ -2,42 +2,138 @@ package chat;
 
 
 import javax.swing.*;
-
+import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 
 public class ChatServer extends JFrame implements Runnable {
 
-	private static int WIDTH = 400;
-	private static int HEIGHT = 300;
-	
-	public ChatServer() {
-		super("Chat Server");
-		this.setSize(ChatServer.WIDTH, ChatServer.HEIGHT);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		createMenu();
-		this.setVisible(true);
-		
-	}
-	
-	private void createMenu() {
-		JMenuBar menuBar = new JMenuBar();
-		JMenu menu = new JMenu("File");
-		JMenuItem exitItem = new JMenuItem("Exit");
-		exitItem.addActionListener((e) -> System.exit(0));
-		menu.add(exitItem);
-		menuBar.add(menu);
-		this.setJMenuBar(menuBar);
-	}
-	
+    private static int WIDTH = 400;
+    private static int HEIGHT = 300;
 
-	public static void main(String[] args) {
-		ChatServer chatServer = new ChatServer();
-	}
+    JTextArea textArea;
+    private int clientNo;
 
-	@Override
-	public void run() {
-		
-	}
+    String currentMessage;
+
+    ArrayList<DataOutputStream> clients;
+
+    public ChatServer() {
+        super("Chat Server");
+        clients = new ArrayList<>();
+        textArea = new JTextArea(30, 30);
+        textArea.setEditable(false);
+        this.setLayout(new BorderLayout());
+        this.add(textArea, BorderLayout.CENTER);
+        this.setSize(ChatServer.WIDTH, ChatServer.HEIGHT);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        createMenu();
+        this.setVisible(true);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+        textArea.append("Started Chat Server on " + dtf.format(now) + "\n");
+
+        Thread t = new Thread(this);
+        t.start();
+    }
+
+    private void createMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        JMenuItem exitItem = new JMenuItem("Exit");
+        exitItem.addActionListener((e) -> System.exit(0));
+        menu.add(exitItem);
+        menuBar.add(menu);
+        this.setJMenuBar(menuBar);
+    }
+
+
+    public static void main(String[] args) {
+        ChatServer chatServer = new ChatServer();
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8000);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                InetAddress inetAddress = socket.getInetAddress();
+                textArea.append("Client " + clientNo + "'s host name is "
+                        + inetAddress.getHostName() + "\n");
+                textArea.append("Client " + clientNo + "'s IP Address is "
+                        + inetAddress.getHostAddress() + "\n");
+                clients.add(new DataOutputStream(socket.getOutputStream()));
+                new Thread(new HandleAClient(socket, ++clientNo)).start();
+                Thread.sleep(1);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    class HandleAClient implements Runnable {
+        private Socket socket; // A connected socket
+        private int clientNum;
+
+        /**
+         * Construct a thread
+         */
+        public HandleAClient(Socket socket, int clientNum) {
+            this.socket = socket;
+            this.clientNum = clientNum;
+        }
+
+        /**
+         * Run a thread
+         */
+        public void run() {
+            try {
+                // Create data input and output streams
+                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+                while (true) {
+                    // Create data input and output streams
+                    // Listen for a connection request
+
+                    String message = inputFromClient.readUTF();
+                    currentMessage = message;
+                    try {
+                        for (int i = 0; i < clients.size(); i++) {
+                            if (i != clientNum - 1)
+                                clients.get(i).writeUTF(this.clientNum + ": " + message);
+                            else
+                                clients.get(i).writeUTF(message);
+                        }
+                    } catch (IOException ioe) {
+                        textArea.append("Connection lost with client " + (clientNo-1) + "\n");
+                        break;
+                    }
+
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        public String currentMessage() {
+            return currentMessage;
+        }
+    }
 }
 
 
